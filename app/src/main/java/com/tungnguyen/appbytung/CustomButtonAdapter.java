54913d4fd3ab1694bc806f38dc3436e8807f859a
Tung Nguyen <tnguyen7@fredhutch.org>
2022-04-17 19:53:36 -0700

    Finish HW2

--- app/src/main/java/com/tungnguyen/appbytung/CustomButtonAdapter.java
+++ app/src/main/java/com/tungnguyen/appbytung/CustomButtonAdapter.java
@@ -6,10 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class CustomButtonAdapter extends ArrayAdapter<Button> {
+    private boolean toastIsShowing = false;
+    private Toast curToast;
+
     public CustomButtonAdapter(Context context, ArrayList<Button> buttons) {
         super(context, 0, buttons);
     }
@@ -28,10 +32,22 @@ public class CustomButtonAdapter extends ArrayAdapter<Button> {
         // Get current button in convertView
         Button btnInView = convertView.findViewById(R.id.firstBtn);
 
-        // Set values
+        // Set button text value
         if (curBtn.getText() != null && curBtn.getText() != "") {
             btnInView.setText(curBtn.getText());
         }
+
+        // Set button default onClick() behavior
+        btnInView.setOnClickListener(view -> {
+            // Make toast appear right away when being clicked
+            if(toastIsShowing) {
+                curToast.cancel();
+            }
+
+            curToast = Toast.makeText(getContext(), btnInView.getText().toString(), Toast.LENGTH_LONG);
+            curToast.show();
+            toastIsShowing = true;
+        });
 
         return convertView;
     }
