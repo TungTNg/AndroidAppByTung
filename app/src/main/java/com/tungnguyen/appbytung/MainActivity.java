35cc00ee81162f8d4e1d03b43c37ea7c84a4809d
Tung Nguyen <tnguyen7@fredhutch.org>
2022-05-17 14:06:07 -0700

    Finish HW6

--- app/src/main/java/com/tungnguyen/appbytung/MainActivity.java
+++ app/src/main/java/com/tungnguyen/appbytung/MainActivity.java
@@ -1,19 +1,51 @@
 package com.tungnguyen.appbytung;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText userNameInput;
+    private EditText userEmailInput;
+    private EditText userPasswordInput;
+
+    private SharedPreferencesHelper sharedPreferencesHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate a SharedPreferencesHelper class
+        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        sharedPreferencesHelper = new SharedPreferencesHelper(sharedPreferences);
+
+        // Login input fields
+        userNameInput = findViewById(R.id.userNameInput);
+        userEmailInput = findViewById(R.id.userEmailInput);
+        userPasswordInput = findViewById(R.id.userPasswordInput);
+
+        userNameInput.setText(sharedPreferencesHelper.getEntry("name"));
+        userEmailInput.setText(sharedPreferencesHelper.getEntry("email"));
+        userPasswordInput.setText(sharedPreferencesHelper.getEntry("password"));
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(v -> signIn());
 
         // Get grid
         GridView mainGrid = findViewById(R.id.mainGrid);
@@ -32,4 +64,68 @@ public class MainActivity extends AppCompatActivity {
         mainGrid.setAdapter(buttonArrayAdapter);
     }
 
+    private void signIn() {
+        String userName = userNameInput.getText().toString();
+        String userEmail = userEmailInput.getText().toString();
+        String userPassword = userPasswordInput.getText().toString();
+
+        if (!formIsValid(userName, userEmail, userPassword)) {
+            return;
+        }
+
+        // store shared preferences
+        sharedPreferencesHelper.saveEntry("name", userName);
+        sharedPreferencesHelper.saveEntry("email", userEmail);
+        sharedPreferencesHelper.saveEntry("password", userPassword);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener(this, task -> {
+                if (task.isSuccessful()) {
+                    // update profile. name is the value entered in UI
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(userName)
+                        .build();
+
+                    if (user != null) {
+                        user.updateProfile(profileUpdates)
+                            .addOnCompleteListener(task1 -> {
+                                if (task1.isSuccessful()) {
+                                    startActivity(new Intent(MainActivity.this, FirebaseActivity.class));
+                                }
+                            });
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Something went wrong...\nOR your login credentials are incorrect!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+    }
+
+    private boolean formIsValid(String userName, String userEmail, String userPassword) {
+        boolean result = true;
+        if (TextUtils.isEmpty(userName)) {
+            userNameInput.setError("Required");
+            result = false;
+        } else {
+            userNameInput.setError(null);
+        }
+
+        if (TextUtils.isEmpty(userEmail)) {
+            userEmailInput.setError("Required");
+            result = false;
+        } else {
+            userEmailInput.setError(null);
+        }
+
+        if (TextUtils.isEmpty(userPassword)) {
+            userPasswordInput.setError("Required");
+            result = false;
+        } else {
+            userPasswordInput.setError(null);
+        }
+
+        return result;
+    }
 }
---
+++ app/src/main/java/com/tungnguyen/appbytung/SharedPreferencesHelper.java
@@ -0,0 +1,40 @@
+package com.tungnguyen.appbytung;
+
+import android.content.SharedPreferences;
+
+public class SharedPreferencesHelper {
+
+    // The injected SharedPreferences implementation to use for persistence.
+    private final SharedPreferences mSharedPreferences;
+
+    /**
+     * Constructor with dependency injection.
+     *
+     * @param sharedPreferences The {@link SharedPreferences} that will be used in this DAO.
+     */
+    public SharedPreferencesHelper(SharedPreferences sharedPreferences) {
+        mSharedPreferences = sharedPreferences;
+    }
+
+    /**
+     * Saves the given string to {@link SharedPreferences}.
+     *
+     * @param key key to associate with saved entry {@link SharedPreferences}.
+     * @param message contains string entry to save to {@link SharedPreferences}.
+     * @return {@code true} if writing to {@link SharedPreferences} succeeded. {@code false}
+     *         otherwise.
+     */
+    public boolean saveEntry(String key, String message){
+        // Start a SharedPreferences transaction.
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(key, message);
+
+        // Commit changes to SharedPreferences & return success/failure result
+        return editor.commit();
+    }
+
+    public String getEntry(String key) {
+        return mSharedPreferences.getString(key, "");
+    }
+
+}
